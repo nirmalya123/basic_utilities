cf842aa3ff07469d04af6ecad8ab74b93abb076b
nirmalya123 <31580771+nirmalya123@users.noreply.github.com>
2018-07-27 01:16:18 +0530

    New version.

--- swap_any_macro.c
+++ swap_any_macro.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 
-#define swap(x,y) do \ 
+#define swap2(x,y) do \ 
    { unsigned char swap_temp[sizeof(x) == sizeof(y) ? (signed)sizeof(x) : -1]; \
      memcpy(swap_temp,&y,sizeof(x)); \
      memcpy(&y,&x,       sizeof(x)); \
      memcpy(&x,swap_temp,sizeof(x)); \
     } while(0)
+
+#define swap(a,b) { __typeof__(a) temp; temp = a; a = b; b = temp; }
 
 int main(int argc, char **argv){
     char a,b;
